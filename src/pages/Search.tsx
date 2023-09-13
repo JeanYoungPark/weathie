9ecfaad8ccb8c275eefd76d16f4dc684ac374e43
jjin0ee <jjin0ee@littlefox.com>
2023-09-13 15:34:31 +0900

    feat : 검색 및 즐겨찾기 리스트

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,5 +1,32 @@
+import { useNavigate } from "react-router";
+import leftArrow from "../assets/images/leftArrow.png";
+import { useCallback } from "react";
+
 export const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    const navigate = useNavigate();
+
+    const onClick = useCallback(() => {
+        navigate("/");
+    }, [navigate]);
+
+    return (
+        <div id="body">
+            <div className="header">
+                <img className="back" src={leftArrow} alt="back" onClick={onClick}/>
+                <h1 className="title">날씨</h1>
+                <span> </span>
+            </div>
+            <div className="wrapper">
+                <p className="searchBar">
+                    <input type="text" placeholder="도시 또는 위치 검색" />
+                    <span className="searchIcon"></span>
+                </p>
+            </div>
+            <ul className="wrapper">
+                <li className="favorite">
+                </li>
+            </ul>
+        </div>
+    )
 }
+               
