cea3fe98223f805fe259c39b36bc906d50391606
jjin0ee <jjin0ee@littlefox.com>
2023-09-26 10:03:49 +0900

    feat: 위치 정보 호출

--- src/pages/Weather.tsx
+++ src/pages/Weather.tsx
@@ -6,11 +6,34 @@ import weatherIcon from "../assets/images/weatherIcon.png";
 import character from "../assets/images/character.png";
 import sampleIcon from "../assets/images/sampleIcon.png";
 import { useNavigate } from "react-router";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
+interface locationType {
+    loaded: boolean;
+    coordinates?: {lat: number; lng: number};
+    error?: {code: number; message: string};
+}
 
 export const Weather = () => {
     const navigate = useNavigate();
+    const [location, setLocation] = useState<locationType>({
+        loaded: false,
+        coordinates: {lat: 0, lng: 0}
+    });
+
+    const onSuccess = useCallback((location: {coords: {latitude: number; longitude: number;}}) => {
+        setLocation({
+            loaded: true,
+            coordinates: {lat: location.coords.latitude, lng: location.coords.longitude}
+        });
+    }, []);
+
+    const onError = useCallback((error: {code:number; message: string}) => {
+        setLocation({
+            loaded: true,
+            error
+        });
+    }, []);
 
     const onClickMenu = useCallback(() => {
         navigate("/search");
@@ -18,6 +41,19 @@ export const Weather = () => {
 
     const onClickReload = useCallback(() => {
         window.location.reload();
+    }, []);
+
+    useEffect(() => {
+        if(!("geolocation" in navigator)){
+            onError({
+                code: 0,
+                message: "Geolocation not supported"
+            })
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+
+        // api 전송
     }, []);
 
     return (
