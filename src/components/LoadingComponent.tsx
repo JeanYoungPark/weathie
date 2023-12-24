6ed072185e4df85d848a500c1057d0fefa161fce
JeanYoungPark <Jean.Young.Park.96@gmail.com>
2023-12-24 13:01:11 +0900

    styles: 로딩 아이콘 적용

--- src/components/LoadingComponent.tsx
+++ src/components/LoadingComponent.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import loading from 'assets/images/loading.gif'
 
 export const LoadingComponent = () => {
 
     return (
-        <div>
-            로딩
+        <div id='loading'>
+            <img src={loading} alt="loading"/>
         </div>
     )
 }
